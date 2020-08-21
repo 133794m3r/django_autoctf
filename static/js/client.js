5f7ee2a32ca2e887239cfaa01ca1ed047a6c0872
Macarthur Inbody <133794m3r@gmail.com>
2020-08-20 22:15:56 -0400

    fixing client.js when there are no files.

--- static/js/client.js
+++ static/js/client.js
@@ -40,12 +40,13 @@ function fetch_chal(challenge_id){
 
 		document.getElementById('hints_container').innerHTML = hint_html;
 		let cols = '<div class="row mb-2"><div class="col-12 text-center mt-3"><h2>Files</h2></div>'
-
-		for (let i = 0; i < challenge.files.length; i++) {
-			cols += `<div class='col-12 text-center mt-3'><a href="/file/${challenge.files[i]}" class="btn btn-primary" target="_blank">${challenge.files[i]}</a></div>`
+		if(challenge.files !== null){
+			for (let i = 0; i < challenge.files.length; i++) {
+				cols += `<div class='col-12 text-center mt-3'><a href="/file/${challenge.files[i]}" class="btn btn-primary" target="_blank">${challenge.files[i]}</a></div>`
+			}
+			cols += '</div>'
+			document.getElementById('description').insertAdjacentHTML('beforeend', cols);
 		}
-		cols += '</div>'
-		document.getElementById('description').insertAdjacentHTML('beforeend', cols);
 		const answer_el = document.getElementById('answer');
 		if(solved){
 			answer_el.value = challenge.flag;
